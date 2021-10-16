eccd669b92cc2eaf53af617117c9ca6a7756867e
Luis Moromisato <moromisato.luis@gmail.com>
2021-10-16 09:17:50 -0300

    added create user screen

---
+++ src/pages/users/create.tsx
@@ -0,0 +1,47 @@
+import { Box, Flex, Heading, Divider, VStack, HStack, SimpleGrid, Button } from "@chakra-ui/react";
+import { Input } from "../../components/Form/Input";
+import { SideBar } from "../../components/Form/Sidebar";
+import { Header } from "../../components/Header";
+
+export default function UserList() {
+  return (
+    <Box>
+      <Header />
+      <Flex w="100%" my="6" maxW={1480} mx="auto" px="6">
+        <SideBar />
+
+        <Box flex="1" borderRadius={8} bg="gray.800" p="8">
+          <Heading size="lg" fontWeight="normal">Criar usuário</Heading>
+
+          <Divider my="6" borderColor="gray.700" />
+        
+          <VStack spacing="8">
+            <SimpleGrid minChildWidth="240px" spacing="8" w="100%">
+              <Input name="name" label="Nome completo"/>
+              <Input name="email" label="E-mail"/>
+            </SimpleGrid>
+
+            <SimpleGrid minChildWidth="240px" spacing="8" w="100%">
+              <Input name="password" label="Senha"/>
+              <Input name="password_confirmation" label="Confirmação da senha"/>
+            </SimpleGrid>
+          </VStack>
+
+          <Flex
+            mt="8"
+            justify="flex-end"
+          >
+            <HStack spacing="4">
+              <Button colorScheme="whiteAlpha">
+                Cancelar
+              </Button>
+              <Button colorScheme="pink">
+                Salvar
+              </Button>
+            </HStack>
+          </Flex>
+        </Box>
+      </Flex>
+    </Box>
+  )
+}
