2685bc1fa5bc43e1c71c1e842727d7658f6c6b35
Luis Moromisato <moromisato.luis@gmail.com>
2021-10-16 17:46:08 -0300

    added responsiveness

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,15 +1,22 @@
 import { Flex, Box, Text, Avatar } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Luís Moromisato</Text>
-        <Text color="gray.300" fontSize="small">
-          moromisato.luis@gmail.com
-        </Text>
-      </Box>
 
+      { showProfileData && (
+        <Box mr="4" textAlign="right">
+         <Text>Luís Moromisato</Text>
+         <Text color="gray.300" fontSize="small">
+           moromisato.luis@gmail.com
+         </Text>
+       </Box>
+      )}
+    
       <Avatar size="md" name="Luís Moromisato" src="https://github.com/moromisato.png" />
 
     </Flex>
