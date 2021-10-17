14a6a9e065545b4b986ac8f02a83c69b4c06b524
Luis Moromisato <moromisato.luis@gmail.com>
2021-10-17 13:53:38 -0300

    added query client provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,18 +3,23 @@ import { ChakraProvider } from '@chakra-ui/react';
 import { theme } from '../styles/theme';
 import { SidebarDrawerProvier } from '../contexts/SidebarDrawerContext';
 import { makeServer } from '../services/mirage';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 if (process.env.NODE_ENV === 'development') {
   makeServer();
 }
 
+const queryClient = new QueryClient();
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <SidebarDrawerProvier>
-        <Component {...pageProps} />
-      </SidebarDrawerProvier>
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider theme={theme}>
+        <SidebarDrawerProvier>
+          <Component {...pageProps} />
+        </SidebarDrawerProvier>
+      </ChakraProvider>
+    </QueryClientProvider>
   )
 }
 
