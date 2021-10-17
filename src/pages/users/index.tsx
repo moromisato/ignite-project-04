532465568e2a49eedbb3affa2f0e781c7b85d409
Luis Moromisato <moromisato.luis@gmail.com>
2021-10-17 14:26:30 -0300

    added axios changed fetch to axios

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -5,12 +5,18 @@ import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import Link from "next/link";
 import { useQuery } from 'react-query';
+import { api } from "../../services/api";
+
+interface User {
+  name: string;
+  email: string;
+  createdAt: string;
+}
 
 export default function UserList() {
 
   const { data, isLoading, isFetching, error } = useQuery('users', async () => {
-    const response = await fetch('http://localhost:3000/api/users')
-    const data = await response.json()
+    const { data } = await api.get<{users: User[]}>('users');
 
     const users = data.users.map(user => {
       return {
